f5c8f5eeae32235c88542c5644ba8c2499d65123
Christopher Budo <CB47557@ex.kroger.com>
2015-08-28 08:22:47 -0400

    first commit, reads in command line input, some parsing written

---
+++ BowlingCalc/BowlingCalc/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 0;
+            int[] scoreByFrame = new int[10];
+            int strikeCount = 0;
+            int turkeyCount = 0;
+
+            var input = args[0];
+            
+            Console.Write("{0} was input",input);
+            Dictionary<int, string[]> map = new Dictionary<int, string[]>();
+            var frames = input.Split('-');
+            if (frames.Count() != 10) throw new InvalidOperationException("Input must have 10 frames");
+            for(int i = 0; i < frames.Count(); i++)
+            {
+                map.Add(i, frames[i].Split());
+            }
+            foreach(var key in map.Keys)
+            {
+                if(map[key].Count()==1)
+                {
+                    strikeCount++;
+                    if (strikeCount > 3)
+                    {
+                        turkeyCount++;
+                    }
+
+                }
+            }
+        }
+    }
+}
